cb8ebef3553449b14a9a7109e7a80b287969b243
borkanie <bogdanboboc1789@yahoo.com>
2025-06-01 17:20:46 +0300

    Adds service implementation for the 2 mains ervices using entity framework.

---
+++ Backend/ParliamentMonitor/ParlimentMonitor.ServiceImplementation/PartyService.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParliamentMonitor.Contracts.Model;
+using ParliamentMonitor.Contracts.Services;
+using ParliamentMonitor.DataBaseConnector;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ParlimentMonitor.ServiceImplementation
+{
+    internal class PartyService : IPartyService<Party>
+    {
+        private readonly AppDBContext dbContext;
+
+        public PartyService(AppDBContext context)
+        {
+            dbContext = context;
+            // Cache parties on startup
+            GetAllParties();
+        }
+
+        /*<inheritdoc/>*/
+        public Party CreateParty(string name, string? acronym = null, string? logoUrl = null, Color? color = null)
+        {
+
+                var newParty = new Party();
+                newParty.Id = Guid.NewGuid();
+                newParty.Name = name;
+                if(acronym != null) 
+                    newParty.Acronym = acronym;
+                if (logoUrl != null)
+                    newParty.LogoUrl = logoUrl;
+                if (color != null)
+                    newParty.Color = (Color)color;
+                if (newParty == null || dbContext.Parties.Any(x => x == newParty))
+                {
+                    return dbContext.Parties.First(x => x == newParty);
+                }
+                dbContext.Parties.Add(newParty);
+                dbContext.SaveChanges();
+                return newParty;
+            
+        }
+
+
+        /*<inheritdoc/>*/
+        public Party? GetParty(Guid id)
+        {
+            return dbContext.Parties.First(x => x.Id == id);
+        }
+
+        public void Update(Party item)
+        {
+            if(dbContext.Parties.Contains(item))
+            {
+                dbContext.Update(item);
+                dbContext.SaveChanges();
+            }  
+        }
+
+        public IList<Party> GetAllParties(bool? isActive = null)
+        {
+            return isActive==null ? dbContext.Parties.Where(x => true).ToList() : 
+                dbContext.Parties.Where(x => x.Active == isActive).ToList();
+        }
+
+        public Party? UpdateParty(Guid id, string? name = null, string? acronym = null, string? logoUrl = null, Color? color = null)
+        {
+            var item = GetParty(id);
+            if (item != null)
+            {
+                item.Name = name ?? item.Name;
+                item.Acronym = acronym ?? item.Acronym;
+                item.LogoUrl = logoUrl ?? item.LogoUrl;
+                item.Color = color ?? item.Color;
+                Update(item);
+            }
+            return item;
+        }
+    }
+}
---
+++ Backend/ParliamentMonitor/ParlimentMonitor.ServiceImplementation/PoliticianService.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParliamentMonitor.Contracts.Model;
+using ParliamentMonitor.Contracts.Services;
+using ParliamentMonitor.DataBaseConnector;
+using System.Drawing;
+using System.Reflection;
+
+namespace ParlimentMonitor.ServiceImplementation
+{
+    internal class PoliticianService : IPoliticianService<Politician>
+    {
+        private AppDBContext dBContext;
+
+        public PoliticianService(AppDBContext context)
+        {
+            dBContext = context;
+            // Cache entries form DB.
+            GetAllPoliticians();
+        }
+
+        /*<inheritdoc/>*/
+        public Politician CreatePolitican(string name, Party party, WorkLocation location, Gender gender, bool isCurrentlyActive = true, string? imageUrl = null)
+        {
+            Politician politician = new Politician();
+            politician.Name = name;
+            politician.Party = party;
+            politician.WorkLocation = location;
+            politician.Gender = gender;
+            politician.Active = isCurrentlyActive;
+            if(imageUrl != null)
+            {
+                politician.ImageUrl = imageUrl;
+            }
+            dBContext.Politicians.Add(politician);
+            dBContext.SaveChanges();
+            return politician;
+        }
+
+        public IList<Politician> GetAllPoliticians(Party? party = null, bool? isActive = null, WorkLocation? location = null, Gender? gender = null)
+        {
+            var query = dBContext.Politicians.AsQueryable();
+
+            if (party != null)
+            {
+                query = query.Where(x => x.Party == party);
+            }
+
+            if (isActive != null)
+            {
+                query = query.Where(x => x.Active == isActive);
+            }
+
+            if (location != null)
+            {
+                query = query.Where(x => x.WorkLocation == location);
+            }
+
+            if (gender != null)
+            {
+                query = query.Where(x => x.Gender == gender);
+            }
+
+            return query.ToList(); // single SQL query executed here
+
+        }
+
+        public Politician? GetPolitician(Guid id)
+        {
+            return dBContext.Politicians.First(x => x.Id == id);
+        }
+
+        public void Update(Politician entity)
+        {
+            if (dBContext.Politicians.Contains(entity))
+            {
+                dBContext.Update(entity);
+                dBContext.SaveChanges();
+            }
+        }
+
+        /*<inheritdoc/>*/
+        public Politician? UpdatePoliticianActivity(Guid id, bool isCurrentlyActive)
+        {
+            var item = GetPolitician(id);
+            if(item != null)
+            {
+                item.Active = isCurrentlyActive;
+                Update(item);
+            }
+            return item;
+        }
+
+        public Politician? UpdatePolitician(Guid id, string? name = null, Party? party = null, WorkLocation? location = null, Gender? gender = null, bool? isCurrentlyActive = null, string? imageUrl = null)
+        {
+            var item = GetPolitician(id);
+            if (item != null)
+            {
+                item.Name = name ?? item.Name;
+                item.Party = party ?? item.Party;
+                item.WorkLocation = location ?? item.WorkLocation;
+                item.Gender = gender ?? item.Gender;
+                item.Active = isCurrentlyActive ?? item.Active;
+                item.ImageUrl = imageUrl ?? item.ImageUrl;
+
+                Update(item);
+            }
+            return item;
+        }
+    }
+}
