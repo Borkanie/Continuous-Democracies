7d8d9d1c89f5114160fe99b9bd3c7a95c26d8512
sanducata <cata.sandu11@gmail.com>
2025-09-14 14:21:50 +0300

    feat: add round cards selected state
    - Add classname library

--- FrontEnd/web/src/components/round-card/RoundCard.tsx
+++ FrontEnd/web/src/components/round-card/RoundCard.tsx
@@ -1,18 +1,34 @@
 import type { Round } from '../../utils/api/rounds';
 import styles from './RoundCard.module.css';
+import classNames from 'classnames';
 
-const { Div, title, description, footer, status, date, icon, header } = styles;
+const {
+  Div,
+  title,
+  description,
+  footer,
+  status,
+  date,
+  icon,
+  header,
+  selected,
+} = styles;
 
 type Props = {
   round: Round;
+  isSelected?: boolean;
+  onSelect?: () => void;
 };
 
 export const RoundCard = (props: Props) => {
-  const { round } = props;
+  const { round, isSelected, onSelect } = props;
 
   // TODO: Use real data from `round` prop when available for date, description, and status
   return (
-    <div className={Div}>
+    <div
+      className={classNames(Div, isSelected ? selected : '')}
+      onClick={onSelect}
+    >
       <div className={header}>
         <h3 className={title}>{round.title}</h3>
       </div>
