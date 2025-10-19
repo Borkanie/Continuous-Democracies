81fb6995c8b472b53c19ce3c1c6f26f7a6f9aea3
Catalin Sandu <cata.sandu11@gmail.com>
2025-10-19 13:49:16 +0300

    feat(rounds-list): add spinner while fetching rounds

--- FrontEnd/web/src/components/rounds-list/RoundsList.tsx
+++ FrontEnd/web/src/components/rounds-list/RoundsList.tsx
@@ -5,11 +5,12 @@ import styles from './RoundsList.module.css';
 import { getAllRounds } from '../../utils/api/rounds';
 import { useNavigate, useParams } from '@tanstack/react-router';
 import classNames from 'classnames';
+import { Spinner } from '../spinner/Spinner';
 
 const { Div, header, roundsContainer, separator, top, bottom } = styles;
 
 export const RoundsList = () => {
-  const { data } = useQuery({
+  const { data, isFetching } = useQuery({
     queryKey: ['rounds'],
     queryFn: getAllRounds,
   });
@@ -18,31 +19,32 @@ export const RoundsList = () => {
   const params = useParams({ strict: false });
   const { roundId } = params;
 
-  if (!data) {
-    // TODO: Add empty state here
-    return <></>;
-  }
-
   return (
     <div className={Div}>
       <div className={header}>
         <h3>Lista legi</h3>
-        <p>{data.length}</p>
+        <p>{data?.length}</p>
       </div>
       <Search />
       <div className={roundsContainer}>
-        <div className={classNames(separator, top)}></div>
-        {data.map((round) => (
-          <RoundCard
-            key={round.voteId}
-            round={round}
-            isSelected={roundId === round.voteId.toString()}
-            onSelect={() => {
-              navigate({ to: `round/${round.voteId}` });
-            }}
-          />
-        ))}
-        <div className={classNames(separator, bottom)}></div>
+        {isFetching ? (
+          <Spinner />
+        ) : (
+          <>
+            <div className={classNames(separator, top)} />
+            {data?.map((round) => (
+              <RoundCard
+                key={round.voteId}
+                round={round}
+                isSelected={roundId === round.voteId.toString()}
+                onSelect={() => {
+                  navigate({ to: `round/${round.voteId}` });
+                }}
+              />
+            ))}
+            <div className={classNames(separator, bottom)} />
+          </>
+        )}
       </div>
     </div>
   );
