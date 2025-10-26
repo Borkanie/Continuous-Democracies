b8cd72025386fe2188ed7d074ab6ac4fce25e101
Borkanie <bogdanboboc1789@yahoo.com>
2025-10-26 08:44:00 +0200

    Uses full URL for backend again and fixes spelling issue in workflow file.

--- FrontEnd/web/vite.config.ts
+++ FrontEnd/web/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://host.docker.internal:8080',
+        target: 'http://backend.democratiacontinua.eu',
         changeOrigin: true,
       },
     },
