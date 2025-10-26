4933e678148d091ef9f4969c16057e3ae8c95743
Borkanie <bogdanboboc1789@yahoo.com>
2025-10-26 10:03:45 +0200

    adds url to allowed hosts

--- FrontEnd/web/vite.config.ts
+++ FrontEnd/web/vite.config.ts
@@ -5,12 +5,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'democratiacontinua.eu',
+      'backend.democratiacontinua.eu', 
+      '.trycloudflare.com'          
+    ],
     proxy: {
       '/api': {
         target: 'http://backend.democratiacontinua.eu',
         changeOrigin: true, 
       },
     },
-    port: 5173,
   },
 });
