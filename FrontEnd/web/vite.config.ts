36727a81af268319386caba4e814205158636138
Borkanie <bogdanboboc1789@yahoo.com>
2025-10-26 09:19:24 +0200

    fake change

--- FrontEnd/web/vite.config.ts
+++ FrontEnd/web/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://backend.democratiacontinua.eu',
-        changeOrigin: true,
+        changeOrigin: true, 
       },
     },
   },
